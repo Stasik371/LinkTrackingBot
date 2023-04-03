8d4b065553bccc3523776671595e9222b8133f3c
Stas <s.s.kovalev.04@mail.ru>
2023-04-03 18:08:21 +0300

    Refactoring MockTests

--- ru.tinkoff/java/bot/src/test/java/telegramBot/commands/ListTests.java
+++ ru.tinkoff/java/bot/src/test/java/telegramBot/commands/ListTests.java
@@ -5,6 +5,8 @@ import com.pengrad.telegrambot.model.Chat;
 import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.SendMessage;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import org.mockito.InjectMocks;
@@ -29,6 +31,9 @@ import static org.mockito.Mockito.when;
 
 public class ListTests {
 
+    private static final Long CHAT_ID = 123L;
+
+    private static final String INVALID_COMMAND = "/foo";
     @Mock
     private LinksClientImpl client = Mockito.mock(LinksClientImpl.class);
     @InjectMocks
@@ -37,21 +42,18 @@ public class ListTests {
     private UserManagerProcessorImpl messageProcessor = new UserManagerProcessorImpl(listCommand);
 
 
-    @Test
-    public void invalidCommandTest() throws NoSuchFieldException, IllegalAccessException {
-        long chatId = 123L;
-
+    private Update reflectionArrange(String messageFieldValue, String lastCommandFieldValue) throws IllegalAccessException, NoSuchFieldException {
         var message = new Message();
         var messageClass = message.getClass();
         var messageField = messageClass.getDeclaredField("text");
         messageField.setAccessible(true);
-        messageField.set(message, "/foo");
+        messageField.set(message, messageFieldValue);
 
 
         var chat = new Chat();
         var chatField = Chat.class.getDeclaredField("id");
         chatField.setAccessible(true);
-        chatField.set(chat, chatId);
+        chatField.set(chat, CHAT_ID);
 
         var messageChatField = messageClass.getDeclaredField("chat");
         messageChatField.setAccessible(true);
@@ -65,16 +67,22 @@ public class ListTests {
 
         var lastCommandField = UserManagerProcessorImpl.class.getDeclaredField("lastCommand");
         lastCommandField.setAccessible(true);
-        lastCommandField.set(messageProcessor, "/foo");
+        lastCommandField.set(messageProcessor, lastCommandFieldValue);
+        return update;
+    }
 
-        SendMessage messageFromListCommand = messageProcessor.process(update);
-        assertThat(chatId, equalTo(messageFromListCommand.getParameters().get("chat_id")));
+
+    @Test
+    @DisplayName("Send special message if user send invalid command")
+    void invalidCommandTest() throws NoSuchFieldException, IllegalAccessException {
+        SendMessage messageFromListCommand = messageProcessor.process(reflectionArrange(INVALID_COMMAND, INVALID_COMMAND));
+        assertThat(CHAT_ID, equalTo(messageFromListCommand.getParameters().get("chat_id")));
         assertThat("Команда не поддерживается, попробуйте /help", equalTo(messageFromListCommand.getParameters().get("text")));
     }
 
     @Test
-    public void processListCommand_fullList() throws NoSuchFieldException, IllegalAccessException {
-        Long chatId = 123L;
+    @DisplayName("List command test OK case")
+    void processListCommand_fullList() throws NoSuchFieldException, IllegalAccessException {
         LinkResponse link1 = new LinkResponse(1L, URI.create("https://github.com/sanyarnd/tinkoff-java-course-2022/"));
         LinkResponse link2 = new LinkResponse(2L, URI.create("https://stackoverflow.com/questions/1642028/what-is-the-operator-in-c"));
         var linksList = new ListLinksResponse(new LinkResponse[]{link1, link2}, 2);
@@ -82,76 +90,26 @@ public class ListTests {
                 link1.url().toString() + "\n" +
                 link2.url().toString() + "\n";
 
-        var message = new Message();
-        var messageClass = message.getClass();
-        var messageField = messageClass.getDeclaredField("text");
-        messageField.setAccessible(true);
-        messageField.set(message, "/list");
-
-
-        var chat = new Chat();
-        var chatField = Chat.class.getDeclaredField("id");
-        chatField.setAccessible(true);
-        chatField.set(chat, chatId);
-
-        var messageChatField = messageClass.getDeclaredField("chat");
-        messageChatField.setAccessible(true);
-        messageChatField.set(message, chat);
-
-        var update = new Update();
-        var updateMessageField = Update.class.getDeclaredField("message");
-        updateMessageField.setAccessible(true);
-        updateMessageField.set(update, message);
-
-
-        var lastCommandField = UserManagerProcessorImpl.class.getDeclaredField("lastCommand");
-        lastCommandField.setAccessible(true);
-        lastCommandField.set(messageProcessor, "/list");
 
         when(client.getAllLinks(anyLong())).thenReturn(linksList);
-        SendMessage messageFromProcessor = messageProcessor.process(update);
+        SendMessage messageFromProcessor = messageProcessor.process(reflectionArrange("/list", "/list"));
         assertThat(messageText, equalTo(messageFromProcessor.getParameters().get("text")));
-        assertThat(chatId, equalTo(messageFromProcessor.getParameters().get("chat_id")));
+        assertThat(CHAT_ID, equalTo(messageFromProcessor.getParameters().get("chat_id")));
     }
 
     @Test
-    public void processListCommand_emptyList() throws NoSuchFieldException, IllegalAccessException {
-        Long chatId = 1234L;
+    @DisplayName("List command test BAD case(no tracked links)")
+    void processListCommand_emptyList() throws NoSuchFieldException, IllegalAccessException {
         String emptyListMessage = "Нет отслеживаемых ссылок";
         ListLinksResponse response = new ListLinksResponse(new LinkResponse[0], 0);
 
-        var message = new Message();
-        var messageClass = message.getClass();
-        var messageField = messageClass.getDeclaredField("text");
-        messageField.setAccessible(true);
-        messageField.set(message, "/list");
 
+        when(client.getAllLinks(CHAT_ID)).thenReturn(response);
 
-        var chat = new Chat();
-        var chatField = Chat.class.getDeclaredField("id");
-        chatField.setAccessible(true);
-        chatField.set(chat, chatId);
-
-        var messageChatField = messageClass.getDeclaredField("chat");
-        messageChatField.setAccessible(true);
-        messageChatField.set(message, chat);
-
-        var update = new Update();
-        var updateMessageField = Update.class.getDeclaredField("message");
-        updateMessageField.setAccessible(true);
-        updateMessageField.set(update, message);
-
-
-        var lastCommandField = UserManagerProcessorImpl.class.getDeclaredField("lastCommand");
-        lastCommandField.setAccessible(true);
-        lastCommandField.set(messageProcessor, "/list");
-
-        when(client.getAllLinks(chatId)).thenReturn(response);
-
-        SendMessage messageFromProcessor = messageProcessor.process(update);
+        SendMessage messageFromProcessor = messageProcessor.process(reflectionArrange("/list", "/list"));
 
         assertThat(emptyListMessage, equalTo(messageFromProcessor.getParameters().get("text")));
-        assertThat(chatId, equalTo(messageFromProcessor.getParameters().get("chat_id")));
+        assertThat(CHAT_ID, equalTo(messageFromProcessor.getParameters().get("chat_id")));
 
     }
 }
