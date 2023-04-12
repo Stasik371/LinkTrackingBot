381e8a833d1fb1bbc86eb60cea543efa87291cff
Stas <s.s.kovalev.04@mail.ru>
2023-04-12 21:16:42 +0300

    refactoring migrations(for tests from 1 stage)

--- ru.tinkoff/java/migrations/V1_CREATE.sql
+++ ru.tinkoff/java/migrations/V1_CREATE.sql
@@ -2,7 +2,7 @@ create sequence if not exists chat_pk_sequence;
 create table chat
 (
     chat_id_pk       bigint primary key not null default nextval('chat_pk_sequence'),
-    telegram_chat_id bigint             not null
+    telegram_chat_id bigint unique      not null
 );
 
 alter sequence chat_pk_sequence owned by chat.chat_id_pk;
@@ -11,8 +11,8 @@ create sequence if not exists link_pk_sequence;
 
 create table link
 (
-    link_id_pk      bigint primary key                                                      not null default nextval('link_pk_sequence'),
+    link_id_pk      bigint primary key                                                            not null default nextval('link_pk_sequence'),
     chat_id         bigint references chat (telegram_chat_id) on update cascade on delete cascade not null,
-    uri             text                                                                    not null,
-    last_checked_at timestamp                                                               not null
+    uri             text                                                                          not null,
+    last_checked_at timestamp                                                                     not null
 );
