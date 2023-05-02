e6f0d19c6b6ee33d68622704c043eecf4e57b98d
Stasik371 <s.s.kovalev.04@mail.ru>
2023-05-03 00:37:45 +0300

    commit for resolving conflicts

--- ru.tinkoff/java/bot/src/main/java/ru/tinkoff/edu/java/bot/BotApplication.java
+++ ru.tinkoff/java/bot/src/main/java/ru/tinkoff/edu/java/bot/BotApplication.java
@@ -5,12 +5,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import ru.tinkoff.edu.java.bot.configuration.BotConfig;
 import ru.tinkoff.edu.java.bot.configuration.ClientsConfig;
+<<<<<<< HEAD
+=======
+import ru.tinkoff.edu.java.bot.webclients.implementations.ScrapperClientImpls;
+>>>>>>> parent of 8b22849 (stage 2(and minor refactoring with app.configuration))
 
 @SpringBootApplication
 @EnableConfigurationProperties({BotConfig.class, ClientsConfig.class})
 public class BotApplication {
     public static void main(String[] args) {
         var ctx = SpringApplication.run(BotApplication.class, args);
+<<<<<<< HEAD
+=======
+        var client = ctx.getBean(ScrapperClientImpls.class);
+        System.out.println(client.getAllLinks(1));
+>>>>>>> parent of 8b22849 (stage 2(and minor refactoring with app.configuration))
     }
 
 }
